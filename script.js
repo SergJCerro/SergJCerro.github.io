203d6b073a2e58a41c7702678bd660de9f1a62c7
SergJCerro <stuasvsstuas@gmail.com>
2021-04-23 10:00:38 -0600

    WDD 230 Week 01

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let date = new Date();
-
-function todaydate() {
-    let hour = date.getHours();
-    var hourText = hour ;
-    var timeOfDay = 'AM';
-    if (hour > 12) {
-        hourText = hour - 12;
-        timeOfDay = 'PM';
-    }
-    document.getElementById("Year").innerHTML = date.getFullYear();
-    document.getElementById("LastUpdate").innerHTML = document.lastModified;
-}
+const date = new Date();
+const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    };
+document.querySelector(
+       "#currentdate"
+     ).innerHTML = new Date().toLocaleDateString("en-us", options);
+document.querySelector("#Year").innerHTML = date.getFullYear();
+ document.querySelector("#Lastupdate").innerHTML = document.lastModified;
