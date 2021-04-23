39a42d0a19688984defc38e3beae09f30d997a05
SergJCerro <stuasvsstuas@gmail.com>
2021-04-23 10:14:21 -0600

    WDD 230 Week 01

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 const date = new Date();
-const options = {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    };
-document.querySelector(
-       "#currentdate"
-     ).innerHTML = new Date().toLocaleDateString("en-us", options);
 document.querySelector("#Year").innerHTML = date.getFullYear();
- document.querySelector("#Lastupdate").innerHTML = document.lastModified;
+ document.querySelector("#LastUpdate").innerHTML = document.lastModified;
