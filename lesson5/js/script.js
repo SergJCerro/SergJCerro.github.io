de09f2edb249adc7cacb34e286a4e877d46e9071
SergJCerro <73044575+SergJCerro@users.noreply.github.com>
2021-05-22 15:10:57 -0600

    WDD

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -1,13 +1,44 @@
-const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const day1 = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const month1 = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 var d = new Date();
-var day = d.getDay(); 
+var day = d.getDay();
 var date = d.getDate();
 var month = d.getMonth();
 var year = d.getFullYear();
-var dateStr = day1[day] + ', ' + date + ' ' + month1[month] + ' ' + year;
+var dateStr = day1[day] + ", " + date + " " + month1[month] + " " + year;
 document.querySelector("#Year").textContent = year;
-document.querySelector("#LastUpdate").textContent = dateStr;;
+document.querySelector("#LastUpdate").textContent = dateStr;
+if (day1[day] == "Friday") {
+  document.getElementById("announce").innerHTML =
+    "Saturday = Preston Pancakes 🥞 in the Park!🏞️  9:00 a.m. Saturday at the city park pavilion.";
+  document.getElementById("announce").style.display = "block";
+} else if (day1[day] == "Saturday"){
+  document.getElementById("announce").textContent = 
+  "Preston Pancakes 🥞 in the Park!🏞️  9:00 a.m. Today At City Park Pavilion"
+} else {
+  document.getElementById("announce").style.display = "none";
+}
 
 function change() {
   var time = new Date().getHours();
@@ -25,26 +56,24 @@ function change() {
     bstyle.backgroundColor = "#4B79A1";
     bstyle.backgroundColor =
       "-webkit-gradient(linear, left bottom, left top, from(#283E51), to(#0A2342))";
-    bstyle.backgroundImage =
-      "url('stars.png')";
+    bstyle.backgroundImage = "url('stars.png')";
   }
 }
-  
+
 function Menu() {
-    document.getElementsByClassName("nav")[0].classList.toggle("responsive");
-  }
-
-  if (day1[day] == 'Friday') {
-    document.getElementById("announce").innerHTML = "Saturday = Preston Pancakes 🥞 in the Park!🏞️  9:00 a.m. Saturday at the city park pavilion.";
-    document.getElementById("announce").style.display = "block";
-}
-else {
-    document.getElementById("announce").style.display = "none";
+  document.getElementsByClassName("nav")[0].classList.toggle("responsive");
 }
 
-  function log() {
-    setTimeout(console.log.bind(console, "%c Sergio J Cerritos %c", "background-color: #bdd4e7;background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);padding:5px;border-radius: 5px;line-height: 26px;", ""));
-    console.log("Hello, This Was Created by");
-  }
-  log();
 
+function log() {
+  setTimeout(
+    console.log.bind(
+      console,
+      "%c Sergio J Cerritos %c",
+      "background-color: #bdd4e7;background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);padding:5px;border-radius: 5px;line-height: 26px;",
+      ""
+    )
+  );
+  console.log("Hello, This Was Created by");
+}
+log();
