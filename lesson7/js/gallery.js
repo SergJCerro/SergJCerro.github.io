df8fcbfc34fa6be09df5d5f9b2dff4bc5cb10dda
SergJCerro <73044575+SergJCerro@users.noreply.github.com>
2021-06-04 20:55:02 -0600

    UPDATE

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -1,4 +1,4 @@
- const days = [
+  /*const days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -27,29 +27,49 @@
   const monatNamen = months[iDatum.getMonth()];
   const jahr = iDatum.getFullYear();
   
-  
   window.localStorage.date = `${iDatum.getDate()} ${tagNamen} ${jahr}`; 
-  const datumein = localStorage.date;
-  const datumzwei = new Date();
+  var datumein = localStorage.date;
+  var datumzwei = new Date();
   
-  const einsTagen = 1000 * 60 * 60 * 24;
-  const date1 = new Date(datumein);
-  const date2 = new Date(datumzwei);
-  const subtraction = date1.getTime() - date2.getTime();
-  const result = Math.round(subtraction / einsTagen);
+  var einsTagen = 1000 * 60 * 60 * 24;
+  //var date1 = new Date(datumein);
+  //var date2 = new Date(datumzwei);
+  var subtraction = datumein - datumzwei;
+  var result = Math.round(subtraction / einsTagen );
   document.querySelector('#figure').textContent = `Days Since Last Visit  ${result} days.`; 
+  console.log(result);
   
 
   if (typeof Storage !== "undefined") {
-    if (localStorage.visits) {
+    if (localStorage.visit) {
       document.querySelector("#day").textContent =
         "Welcome back, you've been here " +
-        localStorage.visits +
+        localStorage.visit +
         " times before.";
-      localStorage.visits = Number(localStorage.visits) + 1;
+      localStorage.visit = Number(localStorage.visit) + 1;
     } else {
-      localStorage.visits = 1;
+      localStorage.visit = 1;
       document.querySelector("#day").textContent =
         "This is your first time here! Welcome.";
     }
   }
+  */
+ // One day Time in ms (milliseconds)
+ const one_day = 1000 * 60 * 60 * 24
+  
+ // To set present_dates to two variables
+ const visit_date = localStorage.Date;
+   
+ // 0-11 is Month in JavaScript
+ const present_day = new Date()
+   
+   
+ // To Calculate the result in milliseconds and then converting into days
+ const Result = Math.round(present_day.getTime() - present_day.getTime()) / (one_day);
+   
+ const Final_Result = Result.toFixed(0);
+   
+ //To display the final_result value
+ document.querySelector('#display').textContent="Your Last Visit was " 
+                 
+                + Final_Result +" Days ago";
--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -73,12 +73,12 @@ function log() {
   setTimeout(
     console.log.bind(
       console,
-      "%c Sergio J Cerritos %c",
+      "%c Created by : Sergio J Cerritos %c",
       "background-color: #bdd4e7;background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);padding:5px;border-radius: 5px;line-height: 26px;",
       ""
     )
   );
-  console.log("Hello, This Was Created by");
+  
 }
 log();
 
