051e7bc2393786aa9e878d0e3dceb883ac1589d0
SergJCerro <73044575+SergJCerro@users.noreply.github.com>
2021-06-12 19:41:30 -0600

    Update

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -55,3 +55,12 @@ function Menu() {
   document.getElementsByClassName("nav")[0].classList.toggle("responsive");
 }
 
+var elem = document.querySelector('input[type="range"]');
+
+var rangeValue = function(){
+  var newValue = elem.value;
+  var target = document.querySelector('.value');
+  target.innerHTML = newValue;
+}
+
+elem.addEventListener("input", rangeValue);
