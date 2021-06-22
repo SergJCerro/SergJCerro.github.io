93f6c9f8638632cdf4f541fc304ca58d6eb9f620
SergJCerro <73044575+SergJCerro@users.noreply.github.com>
2021-06-21 22:11:15 -0600

    Lol2

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -87,7 +87,7 @@ function log() {
   setTimeout(
     console.log.bind(
       console,
-      "%c Sergio J Cerritos %c",
+      "%c Created By: Sergio J Cerritos %c",
       "background-color: #bdd4e7;background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);padding:5px;border-radius: 5px;line-height: 26px;",
       ""
     )
