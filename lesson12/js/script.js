f2d62dbcfea8b3d280514b16ae1403e55fcd3ee8
SergJCerro <stuasvsstuas@gmail.com>
2021-07-10 21:55:59 -0600

    LELW

--- lesson12/js/script.js
+++ lesson12/js/script.js
@@ -1,19 +1,37 @@
-let lastScrollpos = window.pageYOffset;
-let menuButtonCheck = document.querySelector(".menu-btn");
-window.onscroll = function() {
-	let currentScrollPos = window.pageYOffset;
-	if (lastScrollpos > currentScrollPos) {
-		document.getElementById("top-nav").style.top = "0";
-	} else {
-		document.getElementById("top-nav").style.top = "-75px";
-	}
-	lastScrollpos = currentScrollPos;
-	menuButtonCheck.checked = false;
-} 
 
-//Lines
-jQuery(document).ready(function($){
-    setTimeout(function(){
-        $('.trans--grow').addClass('grow');
-    }, 275);
-});
+function Menu() {
+    document.getElementsByClassName("nav")[0].classList.toggle("responsive");
+  }
+
+  //Slider Js
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+window.onload= function () {
+ setInterval(function(){ 
+     plusSlides(1);
+ }, 6000);
+ }
